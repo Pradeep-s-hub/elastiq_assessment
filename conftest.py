657502d99158b20313f25b6cc41758f267afc26e
Pradeep-s-hub <sompradeep.p@gmail.com>
2025-01-30 13:02:32 +0530

    Update drive loaction

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 @pytest.fixture()
 def setup():
-    service_obj = Service(".\\driver\\chromedriver.exe")
+    service_obj = Service(".\\chromedriver.exe")
     driver = webdriver.Chrome(service=service_obj)
     driver.maximize_window()
     return driver
